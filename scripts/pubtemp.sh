570cd32ebcf97095614a825294f7eca4a7876383
Eliseo Martelli <me@eliseomartelli.it>
2019-07-27 19:57:00 +0200

    fixed bug

--- scripts/pubtemp.sh
+++ scripts/pubtemp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 OUTPUT="$(cat /sys/class/thermal/thermal_zone*/temp)"
-mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -h $MQTT_HOST -m $(( $OUTPUT / 1000)) -t sensors/server/temperature
+/usr/bin/mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -h $MQTT_HOST -m $(( $OUTPUT / 1000)) -t sensors/server/temperature
