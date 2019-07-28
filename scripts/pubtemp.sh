6c9f03a20c36dc48be7618a9d20c8bd71dd19072
Eliseo Martelli <me@eliseomartelli.it>
2019-07-28 21:27:13 +0200

    templates

--- scripts/pubtemp.sh
+++ scripts/pubtemp.sh
@@ -1,3 +1,3 @@
-#!/bin/bash -l
-OUTPUT="$(cat /sys/class/thermal/thermal_zone*/temp)"
-/usr/bin/mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -h $MQTT_HOST -m $(( $OUTPUT / 1000)) -t sensors/server/temperature
+#!/bin/bash
+OUTPUT="$(/bin/cat /sys/class/thermal/thermal_zone*/temp)"
+/usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m $(( $OUTPUT / 1000)) -t sensors/server/temperature
