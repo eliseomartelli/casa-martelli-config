4e9f63dadaba1ea2949ea4b55f9ba3abdd9e3ad3
Eliseo Martelli <me@eliseomartelli.it>
2019-08-07 19:54:54 +0200

    Fix Script

--- scripts/pubmodeminfo.sh
+++ scripts/pubmodeminfo.sh
@@ -12,6 +12,7 @@ send \"exit\n\"
 
 PROCESSED_DATA=$(echo "$DATA" | grep "Data Rate" | sed 's|.*:\ ||g' | tr --delete "#")
 
-/usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m $(echo -e "$PROCESSED_DATA" | sed -n '1p') -t   "sensors/modem/data_up"
-/usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m $(echo -e "$PROCESSED_DATA" | sed -n '2p') -t   "sensors/modem/data_down"
+
+/usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m "$(echo "$PROCESSED_DATA" | sed -n '1p')" -t "sensors/modem/data_up"
+/usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m "$(echo "$PROCESSED_DATA" | sed -n '2p')" -t "sensors/modem/data_down"
 
