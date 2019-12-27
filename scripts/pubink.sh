2cb8cc7b18b8cbd8b8c6c9e5ffa1b989e885518d
Eliseo Martelli <me@eliseomartelli.it>
2019-12-27 07:42:41 +0000

    updated ink status script to follow new firmware.

--- scripts/pubink.sh
+++ scripts/pubink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-OUTPUT="$(snmpwalk -v1 -c public $4 iso.3.6.1.2.1.43.11.1.1.9.0 | sed 's|.*:\ ||g')"
-BLACK=$(echo -e "$OUTPUT" | sed -n '1p')
-YELLOW=$(echo -e "$OUTPUT" | sed -n '2p')
-CYAN=$(echo -e "$OUTPUT" | sed -n '3p')
-MAGENTA=$(echo -e "$OUTPUT" | sed -n '4p')
+OUTPUT="$(snmpwalk -v1 -c public $4 iso.3.6.1.2.1.43.11.1.1.9 | sed 's|.*:\ ||g')"
+BLACK=$(echo -e "$OUTPUT" | sed -n '4p')
+YELLOW=$(echo -e "$OUTPUT" | sed -n '3p')
+CYAN=$(echo -e "$OUTPUT" | sed -n '2p')
+MAGENTA=$(echo -e "$OUTPUT" | sed -n '1p')
 
 /usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m $BLACK -t   "sensors/printer/ink/black"
 /usr/bin/mosquitto_pub -u $1 -P $2 -h $3 -m $YELLOW -t  "sensors/printer/ink/yellow"
