472a9ef33603e8f715f7c7a510e9e898b60ee719
Eliseo Martelli <me@eliseomartelli.it>
2020-07-17 18:44:54 +0200

    fix: trim whitespace

--- .docs/generate_readme.sh
+++ .docs/generate_readme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COUNT=$(find $1/homeassistant/automations -type f | wc -l)
+COUNT=$(find $1/homeassistant/automations -type f | wc -l | xargs)
 VERSION=$(cat $1/homeassistant/.HA_VERSION)
 cat $1/.docs/readme_template.md |
 	sed -e s/%AUTOMATIONS/$COUNT/g |
